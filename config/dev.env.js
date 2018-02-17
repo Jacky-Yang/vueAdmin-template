dc267d03de664e05239c87cd058c3113bf3e0033
yangjie <yangjie3232006@126.com>
2018-02-18 01:02:51 +0800

    change BASE_API to my easy-mock url

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,5 +4,7 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  BASE_API: '"https://easy-mock.com/mock/5950a2419adc231f356a6636/vue-admin"',
+  // BASE_API: '"https://easy-mock.com/mock/5950a2419adc231f356a6636/vue-admin"',
+  // BASE_API: '"http://127.0.0.1:8080/sea/"',
+  BASE_API: '"https://easy-mock.com/mock/5a883b5abf160328124e8200/sea"',
 })
--- config/prod.env.js
+++ config/prod.env.js
@@ -1,5 +1,7 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  BASE_API: '"https://easy-mock.com/mock/5950a2419adc231f356a6636/vue-admin"',
+  // BASE_API: '"https://easy-mock.com/mock/5950a2419adc231f356a6636/vue-admin"',
+  // BASE_API: '"http://127.0.0.1:8080/sea/"',
+  BASE_API: '"https://easy-mock.com/mock/5a883b5abf160328124e8200/sea"',
 }
